c727a16cce2dd586f1897559f7b9e9bc5ba36698
catiesud <catie871@gmail.com>
2026-07-18 12:20:52 -0300

    with footer name

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const footer = document.querySelector("footer");
-
-  // Cria spans dinamicamente
   const ano = new Date().getFullYear();
-  const ultimaModificacao = new Date(document.lastModified).toLocaleDateString("pt-BR", {
-    day: "2-digit",
-    month: "long",
-    year: "numeric"
-  });
-
-  footer.innerHTML = `&copy; ${ano} - Seu Nome | Última atualização: ${ultimaModificacao}`;
+  const ultimaModificacao = new Date(document.lastModified).toLocaleString("pt-BR");
+  
+  footer.innerHTML = `&copy; ${ano} - Catiana Guillaume | Última atualização: ${ultimaModificacao}`;
 });
+
+
+
 
 
